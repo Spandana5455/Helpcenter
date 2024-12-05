55036e56c6dd1c68d6da6033b8c446e12809831d
Spandana5455 <150856217+Spandana5455@users.noreply.github.com>
2024-12-05 11:46:35 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HelpCenter from "./helpcenter";
-import FAQsTroubleshooting from "./FAQsTroubleshooting";
-import HCTroubleshooting from "./HCTroubleshooting";
-import ContactOptions from "./ContactOptions"; 
-import LegalDocs from "./LegalDocs";
+import React from "react";
+import ProductPage from "./ProductPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NewPassword from "./NewPassword";
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HelpCenter />} />
-        <Route path="/faqs" element={<FAQsTroubleshooting />} />
-        <Route path="/hctroubleshooting" element={<HCTroubleshooting />} />
-        <Route path="/contactoptions" element={<ContactOptions />} />
-        <Route path="/legaldocs" element={<LegalDocs />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <ProductPage />
+      <NewPassword />
+    </div>
   );
-};
+}
 
 export default App;
 
